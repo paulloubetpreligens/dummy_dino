dd9fb8c059e92226c23b94ccbbfc5f2a510b8348
Paul Loubet <paulloubet@MacBook-Air-de-Paul.local>
2025-11-30 12:18:59 +0100

    Apply linters

--- dummy_dino/dino.py
+++ dummy_dino/dino.py
@@ -11,17 +11,26 @@ class Dino:
     loss_function: torch.nn.Module
     """loss function."""
 
-
     @torch.no_grad()
     def update_teacher_weights(self, momentum_rate: float) -> None:
         """
         Update the teacher's weights using an Exponential Moving Average (EMA)
         of the student's weights.
         """
-        for teacher_param, student_param in zip(self.teacher.parameters(), self.student.parameters()):
-            teacher_param.data.mul_(momentum_rate).add_(student_param.data, alpha=(1 - momentum_rate))
+        for teacher_param, student_param in zip(
+            self.teacher.parameters(), self.student.parameters()
+        ):
+            teacher_param.data.mul_(momentum_rate).add_(
+                student_param.data, alpha=(1 - momentum_rate)
+            )
 
-    def train(self, lr:float, data_loader: torch.utils.data.DataLoader, epoch_number: int, optimizer_function: torch.optim.Optimizer)->None:
+    def train(
+        self,
+        lr: float,
+        data_loader: torch.utils.data.DataLoader,
+        epoch_number: int,
+        optimizer_function: torch.optim.Optimizer,
+    ) -> None:
         for epoch in range(epoch_number):
             total_loss = 0
 
@@ -30,16 +39,16 @@ class Dino:
                 optimizer.zero_grad()
 
                 out_s = self.student(x_s_batch)
-                
+
                 with torch.no_grad():
                     out_t = self.teacher(x_t_batch)
-                
+
                 loss = self.loss_function(out_s, out_t)
 
                 loss.backward()
-                
+
                 optimizer.step()
-   
+
                 self.update_teacher_weights(momentum_rate=0.6)
-                
+
                 total_loss += loss.item()
